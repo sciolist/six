cad30654041b5da6a5279188e3f0c689078712cc
Daniel Pihlstrom <sciolist.se@gmail.com>
2012-10-27 01:54:07 +0200

    rm comment

--- build-system.sh
+++ build-system.sh
@@ -11,11 +11,11 @@ export LDFLAGS="-s"
 rm -rf $WRK
 mkdir -p $OUT && mkdir -p $WRK && mkdir -p $DL
 
-#./scripts/system/musl.sh
-#./scripts/system/binutils.sh
-#./scripts/system/gcc.sh
-#./scripts/system/make.sh
-#./scripts/system/busybox.sh
-#./scripts/system/linux.sh
+./scripts/system/musl.sh
+./scripts/system/binutils.sh
+./scripts/system/gcc.sh
+./scripts/system/make.sh
+./scripts/system/busybox.sh
+./scripts/system/linux.sh
 ./scripts/system/setup.sh
 
