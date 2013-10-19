f695cb5e6a97777196c4191ce8d4fef7b2016975
Daniel Pihlstrom <sciolist.se@gmail.com>
2013-10-19 22:31:18 +0200

    update binutils

--- scripts/compiler/binutils.sh
+++ scripts/compiler/binutils.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -e
-V=397a64b3
+V=2.32.2
+URL="http://ftp.gnu.org/gnu/binutils/binutils-$V.tar.gz"
 SRC=$( cd "$( dirname "$0" )" && pwd )
 cd $WRK
 
-test  -f $DL/binutils-$V.tar.bz2 || wget http://landley.net/aboriginal/mirror/binutils-$V.tar.bz2 -O $DL/binutils-$V.tar.bz2
-tar -xjf $DL/binutils-$V.tar.bz2
+test -f $DL/binutils-$V.tar.gz || wget $URL -O $DL/binutils-$V.tar.gz
+tar -xf $DL/binutils-$V.tar.gz
 
 cd binutils-$V
-patch -N -p1 < $SRC/patches/binutils.patch 
+sed -e 's/uclibc/musl/g' config.sub > config.sub.tmp && mv config.sub.tmp config.sub
 ./configure --target=$A-unknown-linux-musl --prefix="$OUT" \
             --disable-install-libbfd --disable-werror
 make
--- scripts/system/binutils.sh
+++ scripts/system/binutils.sh
@@ -1,16 +1,17 @@
 #!/bin/sh -e
-V=397a64b3
+V=2.32.2
+URL="http://ftp.gnu.org/gnu/binutils/binutils-$V.tar.gz"
 SRC=$( cd "$( dirname "$0" )" && pwd )
 cd $WRK
 
-test  -f $DL/binutils-$V.tar.bz2 || wget http://landley.net/aboriginal/mirror/binutils-$V.tar.bz2 -O $DL/binutils-$V.tar.bz2
-tar -xjf $DL/binutils-$V.tar.bz2
+test -f $DL/binutils-$V.tar.gz || wget $URL -O $DL/binutils-$V.tar.gz
+tar -xf $DL/binutils-$V.tar.gz
 
 CFLAGS="$CFLAGS -ffunction-sections -fdata-sections"
 LDFLAGS="$LDFLAGS -Wl,--gc-sections"
 
 cd binutils-$V
-patch -N -p1 < $SRC/patches/binutils.patch 
+sed -e 's/uclibc/musl/g' config.sub > config.sub.tmp && mv config.sub.tmp config.sub
 ./configure --prefix=/ --host=$A-unknown-linux-musl \
   --target=$A-unknown-linux-musl \
   --disable-werror --disable-shared --disable-nls --disable-install-libbfd
